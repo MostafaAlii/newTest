7cb69786ce80d3a8df36f8a3b8c6a1f040a3197e
MostafaAlii <mostafa28alii@gmail.com>
2023-11-19 12:07:06 +0200

    first commit

---
+++ resources/views/website/layouts/common/web/_footer.blade.php
@@ -0,0 +1,117 @@
+</div>
+<!-- Body End -->
+{{--<footer class="py-4 bg-dark text-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <p>
+            {{ $settings?->notes }}
+          </p>
+          <div class="social-media-icons">
+            <a href="{{ $settings?->twitter }}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/733/733635.png" width="24px"
+                alt="Twitter" /></a>
+            <a href="{{ $settings?->facebook }}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/3128/3128208.png" width="31px"
+                alt="Facebook" /></a>
+            <a href="{{ $settings?->google }}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/104/104093.png" width="24px"
+                alt="Google" /></a>
+            <a href="{{ $settings?->linkedin }}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/3128/3128219.png" width="24px"
+                alt="LinkedIn" /></a>
+
+            <!-- Add more social media icons as needed -->
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <h5>Useful Links</h5>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#"><span style="font-weight: 800"> </span>About Artisticretouch</a>
+            </li>
+            <li>
+              <a href="#"><span style="font-weight: 800"> </span>Terms & Condition</a>
+            </li>
+            <li>
+              <a href="#"><span style="font-weight: 800"> </span>Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#"><span style="font-weight: 800"> </span>Contact us</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 col-md-6">
+          <h5>Contact Information</h5>
+          <ul class="list-styled">
+            <li style="display: flex; align-items: center">
+              <i class="fa fa-envelope" style="font-size: 28px; color: red; margin-right: 10px"></i>
+              <a href="mailto:{{ $settings?->email }}" style="
+                    color: #909090;
+                    font-weight: 500;
+                    text-decoration: none;
+                  ">{{ $settings?->email }}</a>
+            </li>
+          </ul>
+          <h5>We Accept</h5>
+          <img src="{{ asset('website/resources/imgs/acc.jpg') }}" style="width:75%" class="img-fluid" alt="Accepted Payment Methods" />
+        </div>
+      </div>
+      <div class="mt-3 text-center">
+        <p style="
+              background-color: #191919;
+              color: white;
+              padding: 5px;
+              font-size: 15px;
+            ">
+          Copyright 2023 by <a target="_blank" style="text-decoration: none; color:white;" href="https://www.tag-soft.com"> TagSoft </a> | All Rights Reserved
+        </p>
+      </div>
+    </div>
+</footer>--}}
+
+<div class="bottomBorder2"></div>
+<div style="width:1000px;margin:auto;height:70px;">
+  <div style="width: 780px; float: left; font-size: 13px;"><a href="index.html"><span class="style41"><br><span
+          class="style41">HOME</span></span></a><span class="style41"> | <a href="prices.html"><span
+          class="style41">PRICES</span></a><a href="prices.html"></a> | <a href="order.html"><span
+          class="style41">ORDER</span></a><a href="order.html"></a> | <a href="examples.html"><span
+          class="style41">EXAMPLES</span></a><a href="examples.html"></a> | <a href="tour.html"><span
+          class="style41">TOUR</span></a><a href="tour.html"></a> | <a href="faq.html"><span
+          class="style41">FAQ</span></a><a href="contact.html"></a> | <a href="contact.html"><span
+          class="style41">CONTACT</span></a><a href="offers.html"></a> | <a href="offers.html"><span
+          class="style41">OFFERS</span></a><a href="faq.html"></a> | <a href="about.html"><span
+          class="style41">ABOUT</span></a><a href="photo-retouching.html"></a> | <a href="terms.html"><span
+          class="style41">TERMS</span></a><a href="about.html"></a> | <a href="privacy.html"><span
+          class="style41">PRIVACY</span></a><a href="terms.html"></a> | <a href="copyright.html"><span
+          class="style41">COPYRIGHT</span></a><a href="privacy.html"></a></span></div>
+  <div class="payment-methods"></div>
+</div>
+<div class="borderBottom">
+  <div class="block1"></div>
+  <div class="block2"></div>
+  <div class="block3"></div>
+  <div class="block4"></div>
+  <div class="block5"></div>
+  <div class="block6"></div>
+  <div class="block7"></div>
+  <div class="block8"></div>
+  <div class="block7"></div>
+  <div class="block8"></div>
+  <div class="block7"></div>
+  <div class="block8"></div>
+  <div class="block7"></div>
+  <div class="block14"></div>
+  <div class="block15"></div>
+  <div class="block16"></div>
+  <div class="block17"></div>
+  <div class="block18"></div>
+  <div class="block19"></div>
+  <div class="block20"></div>
+  <div class="block21"></div>
+  <div class="block22"></div>
+  <div class="block23"></div>
+  <div class="block24"></div>
+  <div class="block25"></div>
+  <div class="block26"></div>
+  <div class="block27"></div>
+</div>
+<div
+  style="width:1000px; margin:auto; text-align:right; font-family:Arial; font-size:10px; line-height:20px; color:#b9b9b9; margin-bottom: 40px;">
+  COPYRIGHT © ARTISTICRETOUCH.COM 2020 ALL RIGHTS RESERVED </div>
